302a6ea5aca321b531fbe0af3507a8beb29d2806
Jonathan Sexton <joncsexton@gmail.com>
2018-05-24 04:24:19 -0500

    added hidden to cards & imported shuffle function from starter code

--- js/memory.js
+++ js/memory.js
@@ -1,8 +1,23 @@
 const icons = document.querySelector('.cards');
 
 function removeHidden(evt) {
-    evt.target.classList.remove('hidden');
+    evt.target.firstElementChild.classList.toggle('hidden');
     console.log("working");
 }
 
 icons.addEventListener('click', removeHidden);
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
