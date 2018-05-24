f56d684a1e2781fea692ed7971324f7e68339f72
Jonathan Sexton <joncsexton@.com>
2018-05-24 18:00:40 -0500

    working on adding shuffled cards to card list

--- js/memory.js
+++ js/memory.js
@@ -2,6 +2,8 @@ const icons = document.querySelectorAll(".icon");
 let openCards = [];
 const reset = document.querySelector(".reset");
 const cards = [...document.querySelectorAll(".card")];
+const cardList = document.querySelector('.card-list');
+let shuffledArr = shuffle(cards);
 
 reset.addEventListener("click", gameReset);
 // shuffles the "open cards"
@@ -20,18 +22,28 @@ for (let card of cards) {
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-  var currentIndex = array.length,
+function shuffle(cards) {
+  var currentIndex = cards.length,
     temporaryValue,
     randomIndex;
 
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
   }
 
-  return array;
+  return cards;
+}
+
+// needs work
+shuffle(cards);
+for (let i = 0; i < cards.length; i++) {
+    cardList.innerHTML = "";
+    for (let card of cards) {
+        cardList.appendChild(card);
+        card.classList.remove('open', 'show')
+    }
 }
