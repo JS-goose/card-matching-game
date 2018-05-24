2d726f2d90c86f39d0d7fa9c8d9f7968c73fd194
Jonathan Sexton <joncsexton@.com>
2018-05-24 15:16:16 -0500

    added event listeners to cards

--- js/memory.js
+++ js/memory.js
@@ -1,28 +1,38 @@
-const icons = document.querySelector('.cards-wrapper');
-const cards = [...document.querySelectorAll('.card')];
-const openCards = [];
-const reset = document.querySelector('.reset');
+const icons = document.querySelectorAll(".icon");
+const cards = [...document.querySelectorAll(".card")];
+let openCards = [];
+const reset = document.querySelector(".reset");
 
-reset.addEventListener('click', gameReset);
+reset.addEventListener("click", gameReset);
 // shuffles the "open cards"
 function gameReset() {
-    shuffle(cards);
+  shuffle(cards);
+  openCards = [];
 }
 
-/* Removed the need for a function to toggle the .hidden class, will rework later */
-icons.addEventListener('click',);
+// loop to add event listeners to all icons
+icons.forEach(function(evt){
+    this.addEventListener('click', addOpenCards);
+});
+
+// clicked cards are added to the open cards array for comparison
+function addOpenCards(evt) {
+  openCards.push(evt.target);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
