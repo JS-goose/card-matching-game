9aa4fc2b79c2c2128761f6bcd26b950094979a3a
Jonathan Sexton <joncsexton@gmail.com>
2018-05-26 06:12:36 -0500

    trying to refactor code

--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,10 @@ const reset = document.querySelector(".reset"); // reset button
 const cards = [...document.querySelectorAll(".card")]; //array of cards
 const cardList = document.querySelector(".cards-wrapper"); //card deck
 let openCards = [];
+let matchedCards = [];
 
 document.onLoad = gameStart();
+
 // shuffles cards and resets current game
 reset.addEventListener("click", gameReset);
 
@@ -14,36 +16,72 @@ function gameReset() {
 
 // Starts the game
 function gameStart() {
-  // Loop over existing cards
-  let shuffledArr = shuffle(cards);
+  let shuffledArr = shuffle(cards); // variable to hold shuffled deck
 
+  // Loop over existing cards
   for (let i = 0; i < cards.length; i++) {
     cardList.innerHTML = "";
+    // display shuffled cards
     for (let card of cards) {
       cardList.appendChild(card);
     }
-    // display shuffled cards
   }
 }
 
+// Event listener function for cards - DOES NOT WORK YET
+// function addEars(card) {
+//   card.addEventListener("click", function(card) {
+//     if (openCards.length < 2) {
+//       this.classList.add("open", "show");
+//       openCards.push(card);
+//     }
+//     // shows cards in open cards array for 1 second if there are 2 cards
+//     if (openCards.length == 2) {
+//       setTimeout(function(card) {
+//         openCards.forEach(function(card) {
+//           card.classList.remove("open", "show");
+//         });
+//         openCards = [];
+//       }, 700);
+//     }
+//   });
+// }
+
+// function addOpenCards(card) {
+//   // Displays card icon and pushes to open cards array
+//   if (openCards.length < 2) {
+//     card.classList.add("open", "show");
+//     openCards.push(card);
+//   }
+//   // shows cards in open cards array for 1 second if there are 2 cards
+//   if (openCards.length == 2) {
+//     setTimeout(function() {
+//       openCards.forEach(function(card) {
+//         card.classList.remove("open", "show");
+//       });
+//       openCards = [];
+//     }, 700);
+//   }
+// }
 
 // loop to add event listeners to all cards
 for (let card of cards) {
-  card.addEventListener("click", function(e) {
+  // addEars(card);
+  card.addEventListener("click", function() {
     // Displays card icon and pushes to open cards array
-    if (openCards.length < 2) {
-      card.classList.add("open", "show");
-      openCards.push(card);
-    }
-    // shows cards in open cards array for 1 second if there are 2 cards
-    if (openCards.length == 2) {
-      setTimeout(function() {
-        openCards.forEach(function(card) {
-          card.classList.remove("open", "show");
-        });
-        openCards = [];
-      }, 700);
-    }
+  if (openCards.length < 2) {
+    card.classList.add("open", "show");
+    openCards.push(card);
+  }
+  // shows cards in open cards array for 1 second if there are 2 cards
+  if (openCards.length == 2) {
+    setTimeout(function() {
+      openCards.forEach(function(card) {
+        card.classList.remove("open", "show");
+      });
+      openCards = [];
+    }, 700);
+  }
   });
 }
 
@@ -63,3 +101,6 @@ function shuffle(cards) {
 
   return cards;
 }
+
+// if cards match, add .match class
+    // push to matched cards array
