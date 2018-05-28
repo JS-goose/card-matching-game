e639dbae68ccc0b81fd996780f8588ee14d7134f
Jonathan Sexton <joncsexton@gmail.com>
2018-05-27 19:02:52 -0500

    page reload for reset

--- js/memory.js
+++ js/memory.js
@@ -7,21 +7,49 @@
 - Build timer function
 - Reset timer on game win / game reset
 */
-
-let allCards = document.getElementsByClassName('card'); 
+let allCards = document.getElementsByClassName("card");
 let cards = Array.from(allCards); //array of cards created from allCards
 const cardList = document.querySelector(".cards-wrapper"); //card deck
 let openCards = []; //array of open cards
 let matchedCards = []; //array of matches
 const resetButton = document.querySelector(".reset"); //reset button
 let moves = 0;
-let moveCounter = document.querySelector('.move-counter');
-let stars = [...document.querySelectorAll('.fa-star')];
+let moveCounter = document.querySelector(".move-counter");
+let stars = [...document.querySelectorAll(".fa-star")];
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let totalSeconds = 0;
+
+// Timer functionality
+// snippet from Stack Overflow https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+let timeInterval = setInterval(timer, 1000);
+
+function timer() {
+  totalSeconds++;
+  seconds.innerHTML = pad(totalSeconds % 60);
+  minutes.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
 
 document.onLoad = gameStart();
 
+// Starts the game
+function gameStart() {
+  shuffle(cards);
+  shuffledCards();
+  moves = 0;
+}
+
 // loop over existing cards to display shuffled cards
-function shuffledCards () {
+function shuffledCards() {
   for (let i = 0; i < cards.length; i++) {
     cardList.innerHTML = "";
     // display shuffled cards
@@ -31,53 +59,47 @@ function shuffledCards () {
   }
 }
 
-// Starts the game
-function gameStart() {
-  shuffledCards();
-  shuffle(cards);
-  moves = 0; 
-}
-
 // moves counter functionality
-function movesDisplay () {
+function movesDisplay() {
   // if moves = 8 display 3 stars
   if (moves > 8) {
-    stars[4].classList.add('hidden');
-  } 
+    stars[4].classList.add("hidden");
+  }
   if (moves > 12) {
-    stars[3].classList.add('hidden');
-  } 
+    stars[3].classList.add("hidden");
+  }
   if (moves > 16) {
-    stars[2].classList.add('hidden');
+    stars[2].classList.add("hidden");
   }
   if (moves === 20) {
-    stars[1].classList.add('hidden');
-  } 
+    stars[1].classList.add("hidden");
+  }
   // if moves <= 16 display 2 stars
   // if moves <= 20 display 1 star
 }
 
-// shuffles cards and resets current game
+// resets current game
 resetButton.addEventListener("click", gameReset);
 
 // reset functionality
-function gameReset() { 
-  moves = 0;
-  moveCounter.innerHTML = moves;
-  gameStart();
-  openCards.forEach(function (card){
-    card.classList.remove('open','match','unclick','show');
-    openCards = [];
-  })
-  matchedCards.forEach(function (card){
-    card.classList.remove('open','match','unclick','show');
-    matchedCards = [];
-  })
-  // restarts the timer
-  // resets number of stars
-  stars.forEach(function (star) {
-  star.classList.remove('hidden');
-  })
+function gameReset() {
+  location = location;
+  // shuffledCards();
+  // gameStart();
+  // moves = 0;
+  // moveCounter.innerHTML = moves;
+  // openCards.forEach(function(card) {
+  //   card.classList.remove("open", "match", "unclick", "show");
+  //   openCards = [];
+  // });
+  // matchedCards.forEach(function(card) {
+  //   card.classList.remove("open", "match", "unclick", "show");
+  //   matchedCards = [];
+  // });
+  // // resets number of stars
+  // stars.forEach(function(star) {
+  //   star.classList.remove("hidden");
+  // });
 }
 
 // loop to add event listeners to all cards
